71f9d3758015ce1fa27de3b3a38b5e467f1755b9
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-12 01:26:20 +0600

    Watermelon

---
+++ c++/4A.cpp
@@ -0,0 +1,21 @@
+    // Submit Date: 12/07/24
+     
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+        int a, temp, temp2, rem, div;
+        cin >> a;
+     
+        if (a % 2 == 0)
+        {
+            if (a / 2 == 1)
+            {
+                cout << "NO" << endl;
+            }
+            else
+                cout << "YES" << endl;
+        }
+        else
+            cout << "NO" << endl;
+    }
