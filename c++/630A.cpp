bc68e877e644e6738dea6a40e928379d3ebac21d
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2024-08-09 19:41:00 +0600

    630A - Again Twenty Five!

---
+++ c++/630A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    ll input; 
+    
+    cin >> input;
+    cout << 25 << endl;
+    
+    return 0;
+}
