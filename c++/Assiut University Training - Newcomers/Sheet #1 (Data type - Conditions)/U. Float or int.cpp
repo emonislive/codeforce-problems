bb8b38f572cdf4bc4f47a1b9a23f8020009e4d81
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-12 19:54:32 +0600

    219158U

--- c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/U. Float or int.cpp
+++ c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/U. Float or int.cpp
@@ -6,20 +6,21 @@ using namespace std;
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);
+	
 	int condition1, condition2;
 	double input, fOutput;
 	cin >> input;
   
-	condition1 = ceil(input);				  // ceiling math
-	condition2 = floor(input);				// floor math
+	condition1 = ceil(input);	// ceiling math
+	condition2 = floor(input);	// floor math
 	fOutput = input - condition2; 
   
 	// ex: if input - 3.01 then ceil = 4, floor = 3
 	if (condition1 == condition2)			
-		cout << "int " << condition1 << endl;
+	    cout << "int " << condition1 << endl;
 	// ex: if input - 3.0 then ceil = 3, floor = 3
-  else									            
-		cout << "float " << condition2 << " " << fOutput << endl;
+  	else									            
+	    cout << "float " << condition2 << " " << fOutput << endl;
 
 	return 0;
 }
