dd5379b099ed8c5cd802523f3d8b49af219b6a66
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-12 19:37:40 +0600

    219158I

--- c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions.cpp
+++ c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions.cpp
@@ -8,11 +8,9 @@ signed main()
 	ios::sync_with_stdio(false); cin.tie(NULL);
 	short a, b;
 	cin >> a >> b;
-
 	if (a > b || a == b)
 		cout << "Yes" << endl;
 	else
 		cout << "No" << endl;
-	
 	return 0;
 }
