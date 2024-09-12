0f683bc8588e674f92602d7f15fb764bd97979e1
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-12 19:38:08 +0600

    219158J

--- c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/J. Multiples.cpp
+++ c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/J. Multiples.cpp
@@ -7,13 +7,11 @@ signed main(){
 	ios::sync_with_stdio(false); cin.tie(NULL);
 	int a, b;
 	cin >> a >> b;
-	
 	if (a % b == 0)
 		cout << "Multiples" << endl;
 	else if (b % a == 0)
 		cout << "Multiples" << endl;
 	else
 		cout << "No Multiples" << endl;
-	
 	return 0;
 }
