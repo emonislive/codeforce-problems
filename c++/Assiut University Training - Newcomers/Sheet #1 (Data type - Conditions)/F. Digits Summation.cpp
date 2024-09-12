4f826150e046675a31581a29a0d2ffc657050d98
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-12 19:34:13 +0600

    219158F

--- c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
+++ c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
@@ -6,13 +6,10 @@ using namespace std;
 signed main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);
- 
 	int a, b, num1, num2;
 	cin >> a >> b;
- 
 	num1 = a % 10;
 	num2 = b % 10;
-
 	cout << num1 + num2 << endl;
 	return 0;
 }
