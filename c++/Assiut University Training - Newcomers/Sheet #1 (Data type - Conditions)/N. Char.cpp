9bfec1ac9a9b385d58aebaee8781149ee366858e
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-12 19:45:02 +0600

    219158N

--- c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/N. Char.cpp
+++ c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/N. Char.cpp
@@ -7,16 +7,14 @@ int main()
 	char a;
 	cin >> a;
 
-	if (a >= 65 && a <= 90)
-  {
+	if (a >= 65 && a <= 90) {
 		a = a + 32;
 		cout << a << endl;
-  }
-	else if (a >= 97 && a <= 122)
-  {
+  	}
+	else if (a >= 97 && a <= 122) {
 		a = a - 32;
 		cout << a << endl;
-  }
+  	}
 	else
 		exit(0);
 
