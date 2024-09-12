8acebd0e857ddfb3c3e7506765c53f04ba36a626
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-12 19:36:29 +0600

    219158G

--- c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/G. Summation from 1 to N.cpp
+++ c++/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/G. Summation from 1 to N.cpp
@@ -8,8 +8,8 @@ signed main()
 	ios::sync_with_stdio(false); cin.tie(NULL);
 	int a, sum = 0;
 	cin >> a;
-	
-  sum = (a * 1ll * (a + 1)) / 2; // 1ll used to avoid overflow, 64 bits supports
+	// 1ll used to avoid overflow, 64 bits supports
+  	sum = (a * 1ll * (a + 1)) / 2; 
 	cout << sum << endl;
 	return 0;
 }
