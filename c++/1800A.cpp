ac6d1ab6e1767535fbaa595605892941bab69926
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-15 02:31:31 +0600

    A. Is It a Cat?

--- c++/1800A.cpp
+++ c++/1800A.cpp
@@ -7,8 +7,8 @@ void solve(int n, string s) {
 
     transform(s.begin(), s.end(), s.begin(), ::tolower);
     // unique sorts the unique values left side and sets the duplicates on the right side,
-    // ex. 1 2 3 1 4 1 0, unique: 1 2 3 4 0 1 1 1
-    // erase remove the duplicates -> 1 1 1
+    // ex. 1 2 3 1 4 4 1 0, unique: 1 2 3 4 0 1 1 4
+    // erase remove the duplicates -> 1 1 4
     s.erase(unique(s.begin(), s.end()), s.end());
     
     if (s == "meow")
