69078e009b6f062a506f24945a9d5c1b237c9509
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-30 02:25:58 +0600

    B. Different String

--- c++/1971B.cpp
+++ c++/1971B.cpp
@@ -2,35 +2,31 @@
 using namespace std;
 #define endl '\n'
 
-void solve(int t)
-{
+void solve() {
     string s;
+    
     cin >> s;
     string s2 = s;
-    for (int i = 0; i < s.length() - 1; i++)
-    {
+    
+    for (int i = 0; i < s.length() - 1; i++) {
         swap(s[i], s[i + 1]);
     }
     if (s == s2)
         cout << "NO" << endl;
-    else
-    {
+    else {
         cout << "YES" << endl;
         cout << s << endl;
     }
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
     int t;
     cin >> t;
-    while (t--)
-    {
-        solve(t);
+    
+    while (t--) {
+        solve();
     }
     return 0;
 }
