91e94a6bb2a9e21144c60a8082984b47f188cd9b
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-10-20 02:17:01 +0600

    A. Watermelon

--- c++/4A.cpp
+++ c++/4A.cpp
@@ -1,14 +1,10 @@
-    // Submit Date: 12/07/24
-     
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int a;
     cin >> a;
-     
-    if (a % 2 == 0)
-    {
+    if (a % 2 == 0) {
         if (a / 2 == 1)
             cout << "NO" << endl;
         else
