dc769d08ca8b7b13b25a76a5f61afd696e47faaf
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-10-20 02:14:47 +0600

    A - Next Round

--- c++/158A.cpp
+++ c++/158A.cpp
@@ -7,20 +7,17 @@ void solve(int size, int position)
 {
     vector<int> vec(size);
     int count = 0;
-    // user input
-    for (int i = 0; i < size; i++)
-    {
+
+    for (int i = 0; i < size; i++) {
         cin >> vec[i];
     }
     // [positive - 1] is used because vector indexing starts from 0 rather than 1.
     int score = vec[position - 1];
 
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         // check if the value is positive integer or not.
         // positive int starts from: 1,2,3..........(inf.)
-        if (vec[i] > 0)
-        {
+        if (vec[i] > 0) {
             if (score <= vec[i])
                 count++;
         }
@@ -30,8 +27,7 @@ void solve(int size, int position)
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(NULL);
 
     int size, position;
     cin >> size >> position;
