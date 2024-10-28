61226a8723b521dda43b2e74b807a9ea65b28643
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-10-29 00:24:35 +0600

    219856O

---
+++ c++/Assiut University Training - Newcomers/Sheet #4 (Strings)/O. Sort String.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void solve(int stringSize) {
+    vector<int> charCount(26, 0);
+    char charInput;
+    for (int i = 0; i < stringSize; i++) {
+        cin >> charInput;
+        // Convert character to an index (0-25) based on 'a'
+        int charToVectorAddress = charInput - 'a';
+        // Increment the frequency of the character
+        charCount[charToVectorAddress]++;
+    }
+    for (int i = 0; i < 26; i++) {
+        // If the current character (represented by index `i`) exists in the input
+        if (charCount[i] != 0) {
+            while (charCount[i]--) {
+                // Convert the index back to a character
+                char vectorAddressToChar = i + 97;
+                cout << vectorAddressToChar;
+            }
+        }
+    }
+}
+
+int main() {
+    fastIO();  // for fast input/output
+    int stringSize;
+
+    cin >> stringSize;
+    solve(stringSize);
+
+    return 0;
+}
