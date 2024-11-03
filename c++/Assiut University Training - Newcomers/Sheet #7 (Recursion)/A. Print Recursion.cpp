2161c158cceb6cd37f47fbfc08874368ef904f84
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-11-03 13:58:11 +0600

    223339A

---
+++ c++/Assiut University Training - Newcomers/Sheet #7 (Recursion)/A. Print Recursion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+void solve(int testCase)
+{
+    if (testCase == 0)
+        return;
+
+    cout << "I love Recursion" << endl;
+    return solve(--testCase);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+  
+    int testCase;
+  
+    cin >> testCase;
+    solve(testCase);
+  
+    return 0;
+}
