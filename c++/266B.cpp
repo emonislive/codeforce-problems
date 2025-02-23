21cf0065ff44c74e2439e56f16297bf619acd806
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-23 21:11:26 +0600

    B. Queue at the School

--- c++/266B.cpp
+++ c++/266B.cpp
@@ -19,6 +19,7 @@ void solve(int lineSize, int seconds) {
         for (int i = 0; i < lineSize - 1; i++) {
             if (totalTime == 0 || i == lineSize - 1)
                 break;
+            
             if (line[i] == 'B' && line[i + 1] == 'G') { // ? B = boy, G = girl [in the line]
                 swap(line[i], line[i + 1]);
                 totalTime--;
