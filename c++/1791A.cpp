1b8be7a7f6ae585cdaccd6f87b04bc6d2f3cdabf
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-23 16:17:38 +0600

    A. Codeforces Checking

--- c++/1791A.cpp
+++ c++/1791A.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #define endl '\n'
+#define ll long long
 
 void solve(char alphabet)
 {
@@ -29,7 +30,7 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int test;
+    ll test;
     char alphabet;
     cin >> test;
     while(test--){
