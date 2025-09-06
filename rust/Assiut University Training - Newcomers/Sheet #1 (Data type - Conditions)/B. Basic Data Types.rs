a45a2f69b989152c8c6624f2d2ba041e02fd0d34
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-06 10:53:07 +0600

    219158B

---
+++ rust/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/B. Basic Data Types.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn main() {
+    let integer: i32;
+    let long_long: i64;
+    let character: char;
+    let float: f32;
+    let double: f64;
+
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).unwrap();
+
+    let arr: Vec<&str> = user_input.split_whitespace().collect();
+
+    integer = arr[0].parse::<i32>().unwrap();
+    long_long = arr[1].parse::<i64>().unwrap();
+    character = arr[2].parse::<char>().unwrap();
+    float = arr[3].parse::<f32>().unwrap();
+    double = arr[4].parse::<f64>().unwrap();
+
+    print!(
+        "{}\n{}\n{}\n{}\n{}",
+        integer, long_long, character, float, double
+    );
+}
