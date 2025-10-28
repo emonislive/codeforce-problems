769fc72eea117daa43de76b17e5a37cf026f6c9f
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-10-28 22:13:21 +0600

    A. Square?

---
+++ c++/2167A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+void fastIO()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+  
+    if (a == b && b == c && c == d)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main()
+{
+    fastIO();
+    ll tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
