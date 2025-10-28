ed33b48572b147a4c56a8a86fe69ce18d135ea29
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-10-28 22:15:36 +0600

    B. Your Name

---
+++ c++/2167B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+void fastIO()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve()
+{
+    ll n;
+    string a, b;
+    
+    cin >> n >> a >> b;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    
+    if (a == b)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main()
+{
+    fastIO();
+    ll tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
