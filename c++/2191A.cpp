155f804a0b59a324198893938f2c1b4d6e82f09c
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2026-01-20 16:43:44 +0600

    A. Array Coloring
    Implement a solution that checks for alternating colors in pairs of integers.

---
+++ c++/2191A.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define endl '\n'
+
+void fastIO() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+}
+
+void solve() {
+    int size{};
+    std::cin >> size;
+    std::vector<std::pair<int, int>> arr(size);
+
+    int index{};
+    for (auto& it : arr) {
+        std::cin >> it.first;
+
+        if (index % 2 == 0)
+            it.second = 0; // ? 0 = RED COLOR
+        else
+            it.second = 1; // ? 1 = BLUE COLOR
+
+        index++;
+    }
+    sort(arr.begin(), arr.end());
+
+    bool flag = false;
+    int temp{};
+
+    index = 0;
+    for (auto& it : arr) {
+        if (index == 0)
+            temp = it.second;
+        else {
+            if (it.second == temp) {
+                flag = true;
+                break;
+            }
+            temp = it.second;
+        }
+        index++;
+    }
+
+    if (flag)
+        std::cout << "nO" << endl;
+    else
+        std::cout << "yEs" << endl;
+}
+
+int main() {
+    fastIO();
+    int tt;
+    std::cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
